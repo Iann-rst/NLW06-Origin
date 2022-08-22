201ff154bdea58f0d85a23240223272ac0b3395e
Iann-rst <iannrst11@gmail.com>
2022-08-22 11:29:57 -0300

    feat: depoimentos, contatos. Swiper e scrollreveal

--- main.js
+++ main.js
@@ -32,3 +32,37 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+/* Testimonials carrossel swiper */
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+
+})
+
+/** SCROLL REVEAL - Mostrar elementos quando der scroll na página */
+
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+});
+
+scrollReveal.reveal(`
+#home .image,  
+#home .text,
+#about .image,
+#about .text,
+#services header, 
+#services .card,
+#testimonials header,
+#testimonials .testimonials,
+#contact .text,
+#contact .links
+  
+`, { interval: 100 })
